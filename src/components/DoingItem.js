0c271c549d1569f2991fcd9dc83b747f8d146e26
Riccardo Mannino <80473943+RiccardoMannino@users.noreply.github.com>
2025-02-22 12:22:49 +0100

    creazione componente reorder list

--- src/components/DoingItem.js
+++ src/components/DoingItem.js
@@ -1,31 +1,12 @@
-import { Button } from "./Button";
+
 import { useList } from "../context/ListContext";
-import { Reorder } from "motion/react"
+import ReorderList from "./ReorderList";
 
 export function DoingItem(){
   const {doing , reorderDoingItems , doneDoing} = useList()
 
 
-  return ( <Reorder.Group values={doing} className="flex h-full items-center justify-center w-full flex-col gap-3.5 text-neutral-50 font-medium" onReorder={(ordine) =>reorderDoingItems(ordine)}>
-<p className="text-xl">Attività in corso</p>
-<div className="flex phone:w-full flex-col h-full items-center justify-between overflow-y-scroll">
-<div className="flex flex-col gap-3.5 phone:self-center self-start">
-				{doing.map((doingItem) => (
-							<Reorder.Item value={doingItem} key={doingItem.id} className="cursor-grab">
-						<span className="mr-2">
-							{doingItem.description}
-						</span>
-								<Button className="text-neutral-50 bg-yellow-500 p-2 font-medium rounded-2xl text-sm hover:scale-110 transition duration-500" onClick={() => doneDoing(doingItem.id)}
-								>
-									✅
-								</Button>
-							</Reorder.Item>
-						))}
-			</div>
-		</div>
-				<p className="text-neutral-50 font-medium text-xl">
-					<span className="mr-1">Stai svolgendo {doing.length} attività!</span>
-				</p>
-  </Reorder.Group>)
+  return (	<ReorderList attività='Attività Svolte' axis="y" values={doing} onReorder={(ordine) => reorderDoingItems(ordine)} typeList={doing} funzione={doneDoing} footer='Stai svolgendo' icon={'✅'}
+	/>)
  
 }
--- src/components/DoneItem.js
+++ src/components/DoneItem.js
@@ -1,31 +1,11 @@
-import { Button } from "./Button";
 import { useList } from "../context/ListContext";
-import { Reorder } from "motion/react"
+import ReorderList from "./ReorderList";
 
 export function DoneItem() {
 	const { done, deleteDoneItems , reorderDoneItems } = useList();
 
 	return (
-			<Reorder.Group axis="y" values={done} className="flex h-full items-center justify-center w-full flex-col gap-3.5 text-neutral-50 font-medium" onReorder={(ordine) => reorderDoneItems(ordine)}>
-				<p className="text-xl">Attività svolte</p>
-				<div className="flex phone:w-full flex-col h-full items-center justify-between overflow-y-scroll">
-					<div className="flex flex-col gap-3.5 phone:self-center self-start">
-						{done.map((doneItem) => (
-							<Reorder.Item value={doneItem} key={doneItem.id} className="cursor-grab">
-						<span className="line-through mr-2">
-							{doneItem.description}
-						</span>
-								<Button className="text-neutral-50 bg-yellow-500 p-2 font-medium rounded-2xl text-sm hover:scale-110 transition duration-500"
-										onClick={() => deleteDoneItems(doneItem.id)}>
-									❌
-								</Button>
-							</Reorder.Item>
-						))}
-					</div>
-				</div>
-				<p className="text-neutral-50 font-medium text-xl">
-					<span className="mr-1">Hai Svolto {done.length} attività!</span>
-				</p>
-			</Reorder.Group>
+			<ReorderList attività='Attività Svolte' axis="y" values={done} onReorder={(ordine) => reorderDoneItems(ordine)} typeList={done} funzione={deleteDoneItems} footer='Hai svolto' icon={'❌'}
+			/>
 	);
 }
